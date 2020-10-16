96a40e401903fef2e4f034a29e49b3ada6c8f6bd
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-10-16 15:35:58 -0400

    added source

---
+++ Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace returnType
+{
+    abstract class Entity
+    {
+        public enum EntityType {TestFunc, HelloWorld};
+        public EntityType type;
+
+        public virtual void Render(bool canPrint){
+            if (canPrint) Console.WriteLine("rendering\n");
+        }
+
+        public virtual EntityType GetEntityType() {
+            return type;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace returnType
+{
+    class MainClass
+    {
+        static Entity[] entities = new Entity[10];
+        static int fps = 60;
+
+        public static void Main(string[] args) {
+
+            entities[0] = new TestFunc();
+            entities[1] = new HelloWorld();
+            MainLoop();
+        }
+
+        static void MainLoop() {
+            do {
+                Thread.Sleep(1000/fps);
+
+                foreach(Entity entity in entities) {
+                    if (entity != null) {
+                        entity.Render(false);
+                        //Console.WriteLine(entity.GetEntityType());
+                    }
+                }
+            }
+            while (true);
+        }
+    }
+
+    class TestFunc : Entity
+    {
+        public TestFunc() {
+            type = EntityType.TestFunc;
+        }
+
+        public override void Render(bool can) {
+            base.Render(can);
+        }
+
+        public override EntityType GetEntityType() {
+            return base.GetEntityType();
+        }
+    }
+
+    class HelloWorld : Entity
+    {
+        public HelloWorld() {
+            type = EntityType.HelloWorld;
+        }
+
+        public override void Render(bool can) {
+            base.Render(can);
+        }
+
+        public override EntityType GetEntityType() {
+            return base.GetEntityType();
+        }
+    }
+}
